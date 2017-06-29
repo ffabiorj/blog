05dcd2c523138ee64b988908884900bff23f1ca9
Fabio Oliveira <fabio20rj@gmail.com>
2017-06-29 09:47:07 -0300

    Add news things

--- app.py
+++ app.py
@@ -1,10 +1,62 @@
-# A project only file
-
+# imports
 
 from flask import Flask, render_template
+from datetime import date
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 
+# instances
 app = Flask(__name__)
+
+# config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# instances
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+# models
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    date_pub = db.Column(db.Date)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, content, date_pub=None):
+        self.title = title
+        if self.date_pub is None:
+            self.date_pub = date.today()
+        self.content = content
+
+    def __repr__(self):
+        return '<Title {}>'.format(self.title)
 
 
 @app.route('/')
@@ -28,5 +80,6 @@ def contact():
     return render_template('contact.html')
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
