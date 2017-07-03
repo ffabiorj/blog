1ae5a543f176ff568df0bc750f6310deac325619
Fabio Oliveira <fabio20rj@gmail.com>
2017-07-03 12:50:17 -0300

    six deploy

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: gunicorn gettingstarted.wsgi --log-file -
 
-web: python app.py runserver 
+web: python app.py runserver --host 0.0.0.0:5000
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """
 A simple page blog
 """
+import os
 import ipdb
 from flask import Flask, render_template, redirect, url_for, flash, \
     request, session
